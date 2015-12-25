5b1a2e76271bcc661aace319fd4cb528805deafc
thesupremecommander <thesupremecmdr@gmail.com>
2015-12-25 02:01:47 -0500

    Rename selection module to ready module.

--- modules/index.js
+++ modules/index.js
@@ -1,4 +1,4 @@
 module.exports = function(app, io, self, server) {
     require('./user')(app, io, self, server);
-    require('./selection')(app, io, self, server);
+    require('./ready')(app, io, self, server);
 };
