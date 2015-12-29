fedef9728a7ffc49cb028fa42edbd5a22ddf47a5
thesupremecommander <thesupremecmdr@gmail.com>
2015-12-29 15:00:29 -0500

    Begin draft module with captain selection.

--- modules/index.js
+++ modules/index.js
@@ -1,4 +1,8 @@
+/* jshint node: true, esversion: 6, eqeqeq: true, latedef: true, undef: true, unused: true */
+"use strict";
+
 module.exports = function(app, io, self, server) {
     require('./user')(app, io, self, server);
     require('./launch')(app, io, self, server);
+    require('./draft')(app, io, self, server);
 };
