7a0897d1c84e3585b40872e604a7a640311e8383
thesupremecommander <thesupremecmdr@gmail.com>
2015-12-29 21:42:53 -0500

    Update chat service.

--- modules/chat.js
+++ modules/chat.js
@@ -4,16 +4,27 @@
 var lodash = require('lodash');
 
 module.exports = function(app, io, self, server) {
-    io.sockets.on('authenticated', function(socket) {
-        socket.on('sendChat', function(message) {
-            let userRestrictions = self.userRestrictions[newAvailability.userID];
+    self.on('sendUserChatMessage', function(chat) {
+        let userRestrictions = self.userRestrictions[chat.userID];
 
-            if (!lodash.includes(userRestrictions.aspects, 'comms')) {
-                io.sockets.emit('chatReceived', {
-                    user: self.getFilteredUser(userID),
-                    message: message
+        if (!lodash.includes(userRestrictions.aspects, 'comms')) {
+            let trimmedMessage = (chat.message || '').trim();
+
+            if (trimmedMessage.length > 0) {
+                io.sockets.emit('chatMessageReceived', {
+                    user: self.getFilteredUser(chat.userID),
+                    message: trimmedMessage
                 });
             }
+        }
+    });
+
+    io.sockets.on('authenticated', function(socket) {
+        socket.on('sendChatMessage', function(message) {
+            self.emit('sendUserChatMessage', {
+                userID: socket.decoded_token,
+                message: message
+            });
         });
     });
 };
