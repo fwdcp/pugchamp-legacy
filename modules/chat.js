6da6d4dc5bc39d55de8b0770574e143884911abb
thesupremecommander <thesupremecmdr@gmail.com>
2016-01-04 00:20:52 -0500

    Update chat to handle connects/disconnects better.

--- modules/chat.js
+++ modules/chat.js
@@ -1,5 +1,6 @@
 /* jshint node: true, esversion: 6, eqeqeq: true, latedef: true, undef: true, unused: true */
 "use strict";
+
 var lodash = require('lodash');
 
 module.exports = function(app, io, self, server) {
@@ -10,42 +11,42 @@ module.exports = function(app, io, self, server) {
             let trimmedMessage = (chat.message || '').trim();
 
             if (trimmedMessage.length > 0) {
-                io.sockets.emit('chatMessageReceived', {
+                io.sockets.emit('messageReceived', {
                     user: self.getFilteredUser(chat.userID),
-                    message: trimmedMessage
+                    body: trimmedMessage
                 });
             }
         }
     });
-	
-	self.on('sendPlayerConnectMessage', function(chat) {
-		let userRestrictions = self.userRestrictions[chat.userID];
 
-		io.sockets.emit('playerConnectReceived', {
-			user: self.getFilteredUser(chat.userID),
-			message: chat.message
-		});
-		
+    self.on('sendSystemMessage', function(message) {
+        if (message.user) {
+            message.user = self.getFilteredUser(message.user);
+        }
 
-	});
+        io.sockets.emit('messageReceived', message);
+    });
+
+    self.on('userConnected', function(userID) {
+        self.emit('sendSystemMessage', {
+            user: userID,
+            action: 'connected'
+        });
+    });
+
+    self.on('userDisconnected', function(userID) {
+        self.emit('sendSystemMessage', {
+            user: userID,
+            action: 'disconnected'
+        });
+    });
 
     io.sockets.on('authenticated', function(socket) {
-
         socket.on('sendChatMessage', function(message) {
-
             self.emit('sendUserChatMessage', {
                 userID: socket.decoded_token,
                 message: message
-
             });
         });
-		socket.on('playerConnected', function(message) {
-			self.emit('sendPlayerConnectMessage', {
-			userID: socket.decoded_token,
-			message: message
-			
-			});
-		
-		});
     });
 };
