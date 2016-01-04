c86dafea2e935b49ada3b904fa4ab6f62479f39e
erynn <erynnb@users.noreply.github.com>
2016-01-03 20:03:26 -0500

    Updated Chat to include connection messages. Also fixed how messages were pushed so app messages can be included as well.

--- modules/chat.js
+++ modules/chat.js
@@ -1,6 +1,5 @@
 /* jshint node: true, esversion: 6, eqeqeq: true, latedef: true, undef: true, unused: true */
 "use strict";
-
 var lodash = require('lodash');
 
 module.exports = function(app, io, self, server) {
@@ -18,13 +17,35 @@ module.exports = function(app, io, self, server) {
             }
         }
     });
+	
+	self.on('sendPlayerConnectMessage', function(chat) {
+		let userRestrictions = self.userRestrictions[chat.userID];
+
+		io.sockets.emit('playerConnectReceived', {
+			user: self.getFilteredUser(chat.userID),
+			message: chat.message
+		});
+		
+
+	});
 
     io.sockets.on('authenticated', function(socket) {
+
         socket.on('sendChatMessage', function(message) {
+
             self.emit('sendUserChatMessage', {
                 userID: socket.decoded_token,
                 message: message
+
             });
         });
+		socket.on('playerConnected', function(message) {
+			self.emit('sendPlayerConnectMessage', {
+			userID: socket.decoded_token,
+			message: message
+			
+			});
+		
+		});
     });
 };
