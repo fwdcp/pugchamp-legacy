4e20220cb0644034dca3da0dcb6565c51b67bd97
thesupremecommander <thesupremecmdr@gmail.com>
2016-01-05 15:59:36 -0500

    Update models.

--- database.js
+++ database.js
@@ -1,69 +1,73 @@
+/* jshint node: true, esversion: 6, eqeqeq: true, latedef: true, undef: true, unused: true */
+"use strict";
+
 var config = require('config');
 var mongoose = require('mongoose');
 
 mongoose.connect(config.get('server.mongodb'));
 
-var userSchema = {
+var userSchema = new mongoose.Schema({
     alias: String,
     steamID: String,
     setUp: {
         type: Boolean,
         default: false
-    },
-    stats: {
-        overall: {
-            games: {
-                played: {
-                    type: Number,
-                    default: 0
-                },
-                wins: {
-                    type: Number,
-                    default: 0
-                },
-                losses: {
-                    type: Number,
-                    default: 0
-                },
-                ties: {
-                    type: Number,
-                    default: 0
-                }
-            },
-            skill: {
-                rating: {
-                    type: Number,
-                    default: 1500
-                },
-                deviation: {
-                    type: Number,
-                    default: 500
-                }
-            }
-        },
-        captain: {
-            games: {
-                played: {
-                    type: Number,
-                    default: 0
-                },
-                wins: {
-                    type: Number,
-                    default: 0
-                },
-                losses: {
-                    type: Number,
-                    default: 0
-                },
-                ties: {
-                    type: Number,
-                    default: 0
-                }
-            }
-        }
     }
-}
+});
+
+var gameSchema = new mongoose.Schema({
+    status: String,
+    date: Date,
+    captains: [{
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        faction: String
+    }],
+    maps: [String],
+    players: [{
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        role: String,
+        team: Number,
+        origin: String,
+        time: Number
+    }],
+    results: [{
+        score: [Number],
+        links: [{
+            type: String,
+            url: String
+        }]
+    }],
+    choices: [{
+        type: String,
+        method: String,
+        captain: Number,
+        faction: String,
+        role: String,
+        player: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        map: String
+    }],
+    pool: {
+        maps: [String],
+        players: [{
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User'
+            },
+            roles: [String]
+        }]
+    }
+});
 
 module.exports = {
+    Game: mongoose.model('Game', gameSchema),
     User: mongoose.model('User', userSchema)
 };
