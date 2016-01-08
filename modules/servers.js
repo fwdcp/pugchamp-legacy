77868acde46af1bac5b4b7ef7bbfb13822d778f7
thesupremecommander <thesupremecmdr@gmail.com>
2016-01-08 15:05:16 -0500

    Check game status in the database.

--- modules/servers.js
+++ modules/servers.js
@@ -6,38 +6,52 @@ var lodash = require('lodash');
 var ms = require('ms');
 var RCON = require('srcds-rcon');
 
+var database = require('../database');
+
 module.exports = function(app, io, self, server) {
     var serverPool = config.get('app.servers.pool');
     var serverTimeout = config.get('app.servers.timeout');
 
     self.on('getAvailableServers', function(callback) {
         Promise.all(lodash.map(serverPool, function(server) {
+            let rcon = RCON({
+                address: server.address,
+                password: server.rcon
+            });
+
             return Promise.race([
-                new Promise(function(resolve, reject) {
-                    let rcon = RCON({
-                        address: server.address,
-                        password: server.rcon
-                    });
-
-                    rcon.connect().then(function() {
-                        return rcon.command('pugchamp_match_info').then(function(result) {
-                            let serverInfo = result.trim().split(' ');
-
-                            if (serverInfo[0] === '0') {
-                                resolve(server);
-                            }
-                            else {
-                                resolve(false);
-                            }
-                        });
-                    }).catch(function() {
-                        resolve(false);
-                    });
+                rcon.connect().then(function() {
+                    return rcon.command('pugchamp_game_info');
                 }),
                 new Promise(function(resolve, reject) {
-                    setTimeout(resolve, ms(serverTimeout), false);
+                    setTimeout(reject, ms(serverTimeout), false);
                 })
-            ]);
+            ]).then(function(result) {
+                let gameID = result.trim();
+
+                if (gameID) {
+                    return new Promise(function(resolve, reject) {
+                        database.Game.findById(gameID, function(err, game) {
+                            if (err) {
+                                resolve(false);
+                                return;
+                            }
+
+                            if (game.status === 'completed' || game.status === 'aborted') {
+                                resolve(false);
+                            }
+                            else {
+                                resolve(server);
+                            }
+                        });
+                    });
+                }
+                else {
+                    return false;
+                }
+            }, function() {
+                return false;
+            });
         })).then(function(results) {
             callback(lodash.compact(results));
         });
