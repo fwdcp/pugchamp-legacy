21f1b39078961bb71eb385e03d45b6c16872ce73
thesupremecommander <thesupremecmdr@gmail.com>
2016-01-10 12:09:46 -0500

    Add stubs for admin pages.

--- modules/admin.js
+++ modules/admin.js
@@ -2,5 +2,15 @@
 "use strict";
 
 module.exports = function(app, io, self, server) {
+    app.get('/admin/users', function(req, res) {
+        // TODO: implement admin page
+    });
 
+    app.get('/admin/games', function(req, res) {
+        // TODO: implement admin page
+    });
+
+    app.get('/admin/servers', function(req, res) {
+        // TODO: implement admin page
+    });
 };
