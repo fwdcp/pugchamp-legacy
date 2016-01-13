4d5e159b0c2e5f18fbae24a137922990e32c5e7e
thesupremecommander <thesupremecmdr@gmail.com>
2016-01-12 22:10:49 -0500

    Update game rating system for new database structure.

--- ratings/rate_game.py
+++ ratings/rate_game.py
@@ -21,53 +21,41 @@ trueskill.setup(mu=rating_base, sigma=rating_base / 3, beta = rating_base / 6, t
 def rate_game(game_id):
     game = db.games.find_one({'_id': ObjectId(game_id)})
 
-    players = game['players']
-    users = {}
-
-    old_team_0_ratings = {}
-    old_team_1_ratings = {}
-
+    old_ratings = []
     weights = {}
 
-    for player in players:
-        user = db.users.find_one(player['user'])
+    for index, team in enumerate(game['teams']):
+        old_team_ratings = {}
 
-        if 'currentRating' in user:
-            rating_info = db.ratings.find_one(user['currentRating'])
-            rating = trueskill.Rating(mu=rating_info['after']['rating'], sigma=rating_info['after']['deviation'])
-        else:
-            rating = trueskill.Rating()
+        for role in team['composition']:
+            for player in role['players']:
+                user = db.users.find_one(player['user'])
 
-        if player['team'] == 0:
-            old_team_0_ratings[user['_id']] = rating
-            if (game['results']['duration'] != 0):
-                weights[(0, user['_id'])] = player['time'] / game['results']['duration']
-        elif player['team'] == 1:
-            old_team_1_ratings[user['_id']] = rating
-            if (game['results']['duration'] != 0):
-                weights[(1, user['_id'])] = player['time'] / game['results']['duration']
+                if 'currentRating' in user:
+                    rating_info = db.ratings.find_one(user['currentRating'])
+                    rating = trueskill.Rating(mu=rating_info['after']['rating'], sigma=rating_info['after']['deviation'])
+                else:
+                    rating = trueskill.Rating()
 
-    if game['results']['score'][0] > game['results']['score'][1]:
-        ranks = (0, 1)
-    elif game['results']['score'][0] < game['results']['score'][1]:
-        ranks = (1, 0)
-    else:
-        ranks = (0, 0)
+                old_team_ratings[user['_id']] = rating
+                if (game['duration'] != 0):
+                    weights[(index, user['_id'])] = player['time'] / game['duration']
 
-    new_team_0_ratings, new_team_1_ratings = trueskill.rate((old_team_0_ratings, old_team_1_ratings), ranks=ranks, weights=weights)
+        old_ratings.append(old_team_ratings)
 
-    for player in players:
-        if player['team'] == 0:
-            old_rating = old_team_0_ratings[player['user']]
-            new_rating = new_team_0_ratings[player['user']]
-        elif player['team'] == 1:
-            old_rating = old_team_1_ratings[player['user']]
-            new_rating = new_team_1_ratings[player['user']]
-        else:
-            continue
+    high_score = max(game['score'])
+    ranks = [high_score - score for score in game['score']]
 
-        result = db.ratings.insert_one({'user': player['user'], 'date': datetime.datetime.now(), 'game': game['_id'], 'before': {'rating': old_rating.mu, 'deviation': old_rating.sigma}, 'after': {'rating': new_rating.mu, 'deviation': new_rating.sigma}})
-        db.users.find_one_and_update({'_id': player['user']}, {'$set': {'currentRating': result.inserted_id}})
+    new_ratings = trueskill.rate(old_ratings, ranks=ranks, weights=weights)
+
+    for index, team in enumerate(game['teams']):
+        for role in team['composition']:
+            for player in role['players']:
+                old_rating = old_ratings[index][player['user']]
+                new_rating = new_ratings[index][player['user']]
+
+                result = db.ratings.insert_one({'user': player['user'], 'date': game['date'], 'game': game['_id'], 'before': {'rating': old_rating.mu, 'deviation': old_rating.sigma}, 'after': {'rating': new_rating.mu, 'deviation': new_rating.sigma}})
+                db.users.find_one_and_update({'_id': player['user']}, {'$set': {'currentRating': result.inserted_id}})
 
 if __name__ == '__main__':
     rate_game()
