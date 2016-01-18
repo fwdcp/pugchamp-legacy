ebe4743779fe76062880bb9a39f92e551e814e45
thesupremecommander <thesupremecmdr@gmail.com>
2016-01-18 00:51:28 -0500

    Actually activate the games module.

--- modules/index.js
+++ modules/index.js
@@ -6,10 +6,10 @@ module.exports = function(app, database, io, self, server) {
     require('./users')(app, database, io, self, server);
     require('./chat')(app, database, io, self, server);
     require('./servers')(app, database, io, self, server);
-    // require('./games')(app, database, io, self, server);
+    require('./games')(app, database, io, self, server);
     require('./draft')(app, database, io, self, server);
     require('./launch')(app, database, io, self, server);
     // require('./admin')(app, database, io, self, server);
     // require('./players')(app, database, io, self, server);
-    require('./champbot')(app, database, io, self, server);	
+    require('./champbot')(app, database, io, self, server);
 };
