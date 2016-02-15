b1fcc5b5f9937a19c4d670037169da8c3f015c0a
thesupremecommander <thesupremecmdr@gmail.com>
2016-02-15 00:56:00 -0500

    Don't perform transitions if the key does not exist.

--- database.js
+++ database.js
@@ -70,21 +70,23 @@ userSchema.set('toObject', {
     getters: true,
     versionKey: false,
     transform(doc, ret) {
-        if (ret.stats.captainScore) {
-            ret.stats.captainScore.low = _.isNumber(doc.stats.captainScore.low) ? math.round(doc.stats.captainScore.low, 3) : null;
-            ret.stats.captainScore.center = _.isNumber(doc.stats.captainScore.center) ? math.round(doc.stats.captainScore.center, 3) : null;
-            ret.stats.captainScore.high = _.isNumber(doc.stats.captainScore.high) ? math.round(doc.stats.captainScore.high, 3) : null;
-        }
+        if (ret.stats) {
+            if (ret.stats.captainScore) {
+                ret.stats.captainScore.low = _.isNumber(doc.stats.captainScore.low) ? math.round(doc.stats.captainScore.low, 3) : null;
+                ret.stats.captainScore.center = _.isNumber(doc.stats.captainScore.center) ? math.round(doc.stats.captainScore.center, 3) : null;
+                ret.stats.captainScore.high = _.isNumber(doc.stats.captainScore.high) ? math.round(doc.stats.captainScore.high, 3) : null;
+            }
 
-        if (ret.stats.rating) {
-            ret.stats.rating.mean = math.round(doc.stats.rating.mean, 0);
-            ret.stats.rating.deviation = math.round(doc.stats.rating.deviation, 0);
-            ret.stats.rating.low = math.round(doc.stats.rating.low, 0);
-            ret.stats.rating.high = math.round(doc.stats.rating.high, 0);
-        }
+            if (ret.stats.rating) {
+                ret.stats.rating.mean = math.round(doc.stats.rating.mean, 0);
+                ret.stats.rating.deviation = math.round(doc.stats.rating.deviation, 0);
+                ret.stats.rating.low = math.round(doc.stats.rating.low, 0);
+                ret.stats.rating.high = math.round(doc.stats.rating.high, 0);
+            }
 
-        if (!doc.options.showDraftStats) {
-            delete ret.stats.draft;
+            if (doc.options && !doc.options.showDraftStats) {
+                delete ret.stats.draft;
+            }
         }
 
         delete ret.options;
@@ -162,8 +164,13 @@ gameSchema.set('toObject', {
     getters: true,
     versionKey: false,
     transform(doc, ret) {
-        ret.map = config.get('app.games.maps')[doc.map];
-        ret.server = _.omit(config.get('app.servers.pool')[doc.server], 'rcon', 'salt');
+        if (doc.map) {
+            ret.map = config.get('app.games.maps')[doc.map];
+        }
+
+        if (doc.server) {
+            ret.server = _.omit(config.get('app.servers.pool')[doc.server], 'rcon', 'salt');
+        }
     }
 });
 
