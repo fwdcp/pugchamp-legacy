4d2ed406f75fd4760631e20b53c2e9c9cbe621bb
tsc <thesupremecmdr@gmail.com>
2016-06-17 01:45:51 -0400

    Improve game update handling.

--- modules/games.js
+++ modules/games.js
@@ -525,14 +525,24 @@ module.exports = function(app, cache, chance, database, io, self) {
 
             yield self.processGameUpdate(game);
 
-            let map = MAPS[game.map].name;
-            let duration = moment.duration(game.duration, 'seconds').format('m:ss', {
-                trim: false
-            });
-            let score = _(game.teams).map((team, index) => `${(HIDE_CAPTAINS || !team.captain) ? team.faction : team.captain.alias} ${game.score[index]}`).join(', ');
+            let action = `[game](/game/${helpers.getDocumentID(game)}) update: live`;
+            if (!_.isNil(game.duration)) {
+                let duration = moment.duration(game.duration, 'seconds').format('m:ss', {
+                    trim: false
+                });
+                action += ` for ${duration}`;
+            }
+            if (game.map) {
+                let map = MAPS[game.map].name;
+                action += ` on ${map}`;
+            }
+            if (game.score) {
+                let score = _(game.teams).map((team, index) => `${(HIDE_CAPTAINS || !team.captain) ? team.faction : team.captain.alias} ${game.score[index]}`).join(', ');
+                action += ` with current score ${score}`;
+            }
 
             self.sendMessage({
-                action: `[game](/game/${helpers.getDocumentID(game)}) update: live for ${duration} on ${map} with current score ${score}`
+                action
             });
         }
         else if (info.status === 'completed') {
@@ -587,14 +597,24 @@ module.exports = function(app, cache, chance, database, io, self) {
             self.removeGameSubstituteRequests(game);
             setTimeout(self.shutdownGameServers, POST_GAME_RESET_DELAY, game);
 
-            let map = MAPS[game.map].name;
-            let duration = moment.duration(game.duration, 'seconds').format('m:ss', {
-                trim: false
-            });
-            let score = _(game.teams).map((team, index) => `${(HIDE_CAPTAINS || !team.captain) ? team.faction : team.captain.alias} ${game.score[index]}`).join(', ');
+            let action = `[game](/game/${helpers.getDocumentID(game)}) update: completed`;
+            if (!_.isNil(game.duration)) {
+                let duration = moment.duration(game.duration, 'seconds').format('m:ss', {
+                    trim: false
+                });
+                action += ` after ${duration}`;
+            }
+            if (game.map) {
+                let map = MAPS[game.map].name;
+                action += ` on ${map}`;
+            }
+            if (game.score) {
+                let score = _(game.teams).map((team, index) => `${(HIDE_CAPTAINS || !team.captain) ? team.faction : team.captain.alias} ${game.score[index]}`).join(', ');
+                action += ` with final score ${score}`;
+            }
 
             self.sendMessage({
-                action: `[game](/game/${helpers.getDocumentID(game)}) update: completed after ${duration} on ${map} with final score ${score}`
+                action
             });
 
             try {
